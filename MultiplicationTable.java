a04f04f1ec823f369934b954083d9658bfbfd40d
Waleed Mortaja <waleed.mortaja@gmail.com>
2017-04-28 02:11:09 +0300

    Create MultiplicationTable.java

---
+++ MultiplicationTable.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the numer you want to get its multiplication table: ");
+        int firstNumber = in.nextInt();
+        System.out.print("What is the  final number you want to reach?  ");
+        int secondNumber = in.nextInt();
+
+        for (int i = 1 ; i <= secondNumber; i++){
+            System.out.println(firstNumber +" * "+ i +" = "+ (firstNumber*i));
+        }
+        
+        
+    }
+
+}
